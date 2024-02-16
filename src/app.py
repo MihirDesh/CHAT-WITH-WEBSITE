b0a699fcb01576f4149a0e771f7dc3b3dd910ac9
MihirDesh <mihirdesh23@gmail.com>
2024-02-17 00:54:17 +0530

    frontend and basic chatbot functionality implemented

---
+++ src/app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+def get_response(user_query):
+    return "I don't know"
+
+def get_vectorstore_from_url(url):
+    loader = WebBaseLoader(url)
+    document = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter()
+    document_chunks = text_splitter.split_documents(document)
+    return document
+
+
+st.set_page_config(page_title="Chat With Websites", page_icon="🤖")
+
+st.title("Chat With Websites")
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+    AIMessage(content="Hello, I am a bot. How can I help you?")
+]
+
+with st.sidebar:
+    st.header("Settings")
+    website_url = st.text_input("Website URL")
+
+if website_url is None or website_url == "":
+    st.info("Please enter a website URL")
+
+else: 
+    document_chunks = get_vectorstore_from_url(website_url)
+    with st.sidebar:
+        st.write(document_chunks)
+
+    user_query = st.chat_input("Type your message here...")
+    if user_query is not None and user_query != "":
+        response = get_response(user_query)
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+        st.session_state.chat_history.append(AIMessage(content=response))
+
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
+            with st.chat_message("AI"):
+                st.write(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message("Human"):
+                st.write(message.content)
+
